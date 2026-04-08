58473457ea3152d39b63f01ccd2fbc0fce4c852b
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-08 11:10:34 +0530

    01_basics - datatypes, primitives, typeof

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";    // treat all js code as newer version
+
+let name = "Saurabh"
+let age  = 18
+let isLoggedIn = true
+
+
+
+// Primitives
+// number has a range which is => 2 to power 53
+// bigint is used for very big numbers(used in trading, quant)
+// string => "" ,  ''
+// boolean => True/False
+// null => it is a standalone value and datatype
+// undefined => means value is not assigned 
+//symbol => unique 
+
+
+
+//Non - primitive dataypes
+
+// object
+
+
+console.log(typeof(name))   // string
+console.log(typeof(undefined))  // undefined
+console.log(typeof(null)) // object
