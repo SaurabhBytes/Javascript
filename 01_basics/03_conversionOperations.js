a4250185bd15916c9de33eb07300d6ba5bda1ba7
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-08 14:30:34 +0530

    01_basics - type conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,60 @@
+let score = "3"
+
+console.log(typeof score)
+
+let valueInNUmber = Number (score)  // we use Number to change the dataype like string or anything else in number
+console.log(typeof valueInNUmber)
+console.log(valueInNUmber)
+
+let score1 = "33abc"
+let score2  = null
+let score3  = undefined
+let score4 =  true
+
+let valueFor = Number(score1)
+let valueIn = Number(score2)
+let valueIs = Number (score3)
+let valueHas = Number(score4)
+
+// console.log(typeof valueFor)  
+// console.log(valueFor)
+// console.log(typeof valueIn)
+// console.log(valueIn)
+// console.log(typeof valueIs)
+// console.log(valueIs)
+// console.log(typeof valueHas)
+// console.log(valueHas)
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 
+
+
+let isLoggedIn = 1
+let isLoggedIn1 = ""
+let  isLoggedIn2 = "hello"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+let booleanIsloggedIn1 = Boolean(isLoggedIn1)
+let booleanIsloggedIn2 = Boolean(isLoggedIn2)
+
+// console.log(booleanIsloggedIn)
+// console.log(booleanIsloggedIn1)
+// console.log(booleanIsloggedIn2)
+
+
+//  1 => true ;  0 =>false
+//  "" => false 
+// "hello " => true 
+
+let isnum = 1
+
+let isString = String(isnum)
+console.log(isString)
+console.log(typeof isString)
+
+
+
+
