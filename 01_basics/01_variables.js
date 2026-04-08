c000886a9ac83a1373e9ca597dfe654315037fdb
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-08 10:47:28 +0530

    01_basics - variables, let ,cosnt, var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,31 @@
+const accountId = 14453
+let accountEmail = "saurabh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// in javascript we can write infront of variable no variable and still store it.
+
+let accountState;
+/*
+
+in javascript if we declare the variable
+without assigning it a value 
+it is cosnidered undefined
+
+
+
+//accountId = 2       // not allowed to change const variable
+
+accountEmail = "Hello@gmail.com"
+accountPassword = "123"
+accountCity = "Bengalure"
+
+/*{
+we dont use var 
+because of issue in block scope and functional scope
+}*/
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
