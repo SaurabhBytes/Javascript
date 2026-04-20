3fc06afd27d9be594f0f357c971666d207d59b24
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-20 16:10:15 +0530

    03_basics - IIFE immediately invoked function expressions

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function  EXPRESSIONS  (IIFE)
+
+(function chai(){
+    console.log(` DB connected`)
+})();
+
+(function aurcode(){
+    console.log(`DB CONNECTED TWO`)
+})();
