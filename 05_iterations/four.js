d4a369e91404e6130e05b5cfb627f52bf50e6867
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-23 12:30:48 +0530

    05_iterations - forEach loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,21 @@
+let arr = [1,2,3,4,5]
+
+arr.forEach(function (item){
+    console.log(item)
+
+})
+
+
+let arr1 = [11,12,13,14,15]
+
+arr1.forEach(item => {
+    console.log(item)
+})
+
+
+
+let arr2 = [21,22,23,24,25]
+
+arr2.forEach((element,index) => {
+    console.log(`${index} : ${element}`)
+})
