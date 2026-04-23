bdc190eb934daa6c31f6fe3763a8fb285d78308f
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-23 12:40:00 +0530

    05_iterations - map

---
+++ 05_iterations/five.js
@@ -0,0 +1,23 @@
+let arr = [1,2,3,4,5]
+
+let newArr = arr.map((item)=>{
+    return item * 3
+})
+
+console.log(newArr);
+console.log(arr);
+
+
+
+let names = ['saurabh', 'rahul', 'priya', 'hitesh']
+
+// let Names = names.map((item) => {
+//     return item.toUpperCase()
+// })
+
+let Names = names.map(item => item.toUpperCase())
+
+console.log(Names)
+
+
+
