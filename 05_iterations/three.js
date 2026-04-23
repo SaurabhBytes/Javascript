bd4018bb1bd2ac7627453f86ca6e0783586896fa
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-04-23 11:38:11 +0530

    05_iterations - for...of and for...in loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,16 @@
+let arr = ['DHURANDHAR', 'ANIMAL', 'Conjuring']
+
+for(let hero of arr){
+    console.log(hero)
+}
+
+
+let user = {
+    name: 'saurabh',
+    age: 23,
+    city:'bilaspur'
+}
+
+for(let key in user){
+    console.log(`${key} : ${user[key]}`)
+}
